9bbf6de2b660bd5fba19b66ff1fc1465b8703154
leodatavinci <leodatavinci@gmail.com>
2022-10-01 14:52:33 +0200

    x

--- 1_📈_Chain_Activity_Dashboard.py
+++ 1_📈_Chain_Activity_Dashboard.py
@@ -10,18 +10,24 @@ from datetime import timedelta
 from charts import *
 
 
-#===========Functions============
+#=============================
+
+mongo_client = pymongo.MongoClient(st.secrets["connection_string"])
+db = mongo_client["Crypto01"]
+
+df_chains = pd.DataFrame(db.chains.find())
+all_chains = list(set(df_chains['coin']))
+    
+#=============================
 
 def draw_option_timespan(invisible_index):
 
         option_timespan = st.selectbox(
         'Time span' + str(invisible_index),
-        ('1 Month', '6 Months', '1 Year', 'All'), 3
+        ('6 Months', '1 Year', 'All'), 2
         )
-        
-        if option_timespan == '1 Month':
-            days = 1 * 31
-        elif option_timespan == '6 Months':
+
+        if option_timespan == '6 Months':
             days = 6 * 31
         elif option_timespan == '1 Year':
             days = 12 * 31
@@ -55,12 +61,11 @@ def draw_chart(df, options, start_date, end_date, title, x_axis, y_axis):
 
 def pie_chart_all_repositories():
     
-    df_repositories_grouped = df_repositories.groupby([pd.Grouper('classification')]).count()
-    df_repositories_grouped = df_repositories_grouped.reset_index(level=0)
-    df_repositories_grouped.loc[df_repositories_grouped['url'] < 2000, 'classification'] = 'Other Chains'
-    df_repositories_grouped.rename(columns={"url": "Total Repositories", "classification": "chain"})
+    df_repository_count = pd.DataFrame(db.repository_count.find())
+    df_repository_count.loc[df_repository_count['Total Repositories'] < 1500, 'chain'] = 'Other Chains'
+    df_repository_count = df_repository_count.reset_index(level=0)
     
-    fig = px.pie(df_repositories, values=df_repositories_grouped['url'], names=df_repositories_grouped['classification'], title='Repositories per Chain Monitored')
+    fig = px.pie(df_repository_count, values=df_repository_count['Total Repositories'], names=df_repository_count['chain'], title='Repositories per Chain Monitored')
     fig.update_traces(textinfo='value')
        
     return fig
@@ -87,8 +92,7 @@ def multiline_chart_commits_all_devs(option_agg, start_date, end_date, options):
     
     return fig
 
-
-def multiline_chart_commits_senior_devs(option_agg, start_date, end_date, options):
+#def multiline_chart_commits_senior_devs(option_agg, start_date, end_date, options):
     
     """
     if option_agg == 'Day':
@@ -133,7 +137,7 @@ def multiline_chart_new_devs_per_chain(start_date, end_date, options):
     
     return fig
 
-def multiline_chart_new_senior_devs_per_chain(start_date, end_date, options):
+#def multiline_chart_new_senior_devs_per_chain(start_date, end_date, options):
            
     df_new_senior_developers_per_chain_per_mo = DataFrame(db.new_senior_developers_per_chain_per_mo.find())
     df_new_senior_developers_per_chain_per_mo['first_date']= pd.to_datetime(df_new_senior_developers_per_chain_per_mo['first_date'])  
@@ -145,79 +149,130 @@ def multiline_chart_new_senior_devs_per_chain(start_date, end_date, options):
     return fig
 
 
- #==========Get Data=============
+ #===============================
  
-mongo_client = pymongo.MongoClient(st.secrets["connection_string"])
-db = mongo_client["Crypto01"]
-
-df_chains = pd.DataFrame(db.chains.find())
-all_chains = list(set(df_chains['coin']))
-
-df_repositories = pd.DataFrame(db.repositories.find())
-df_developers = pd.DataFrame(db.developer.find())
-
- #==========Create Page===========
+def main():
  
-st.set_page_config(
-    page_title="GitHub Repository Monitor",
-    page_icon="📈",
-    layout="wide",
-)
-
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.title("Blockchain Project GitHub Repository Monitor")
-
-st.header("Following the developers to get insights into blockchain demand and building activities.")
-
-#=======================================
-
-with st.container():
+    #==========Create Page===========
     
-    col1, col2 = st.columns(2)
+    st.set_page_config(
+        page_title="GitHub Repository Monitor",
+        page_icon="📈",
+        layout="wide",
+    )
 
-    with col1:
-        
-        st.plotly_chart(pie_chart_all_repositories(), use_container_width=True)
-        
-    with col2:
-        
-        total_repositories = str(len(df_repositories.index))
-        total_developers = str(len(df_developers.index))
-        
-        st.subheader(" ")
-        st.subheader(" ")
-        st.subheader(" ")
-        st.subheader(" ")
-        st.subheader(" ")
-        st.subheader(" ")
-        st.subheader(" ")
-     
-        st.subheader("Total # of Repositories monitored: " + total_repositories)
-        st.subheader("Total # of Developers monitored: " + total_developers)
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
                 
-                       
-with st.container():
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
+    st.title("Blockchain Project GitHub Repository Monitor")
+
+    st.header("Following the developers to get insights into blockchain demand and building activities.")
+
+    #=======================================
+
+    with st.container():
+        
+        col1, col2 = st.columns(2)
+
+        with col1:
+            
+            st.plotly_chart(pie_chart_all_repositories(), use_container_width=True)
+            st.subheader(" ")
+            
+        with col2:
+            
+            df_totals = pd.DataFrame(db.totals.find())
+            
+            total_repositories = str(df_totals.iloc[0]['Total Repositories'])
+            total_developers = str(df_totals.iloc[0]['Total Developers'])
+            
+            st.subheader(" ")
+            st.subheader(" ")
+            st.subheader(" ")
+            st.subheader(" ")
+            st.subheader(" ")
+            st.subheader(" ")
+            st.subheader(" ")
+        
+            st.subheader("Total # of Repositories monitored: " + total_repositories)
+            st.subheader("Total # of Developers monitored: " + total_developers)
+                    
+                        
+    with st.container():
+        
+        options_new_repositories_per_chain = st.multiselect('Select Blockchains:  ', all_chains, ['Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            
+            start_date = datetime.today() - timedelta(days=draw_option_timespan('  '))
+            end_date = datetime.today()      
+
+        with col2:
+            st.write("")
+            
+        with col3:
+            st.write("")
+            
+        with col4:
+            st.write("")
+
+        st.plotly_chart(multiline_new_repositories_per_chain(end_date, start_date, options_new_repositories_per_chain), use_container_width=True)
+
+    st.info('Sum of repositories created per chain per month.', icon="ℹ️")
+
+    #=======================================
+
+    #with st.container():
+
+        #options_commits_sen_devs = st.multiselect('Select Blockchains: ', all_chains, ['Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
+
+        #col1, col2, col3, col4 = st.columns(4)
+
+        #with col1:
+            
+            #start_date = datetime.today() - timedelta(days=draw_option_timespan(' '))
+            #end_date = datetime.today()      
+
+        #with col2:
+
+            #option_agg = st.selectbox(
+            #'Count of Commits per: ',
+            #( 'Week', 'Month'),
+            #1
+            #)
+            
+        #with col3:
+            #st.write("")
+            
+        #with col4:
+            #st.write("")
+
+        #st.plotly_chart(multiline_chart_commits_senior_devs(option_agg, end_date, start_date, options_commits_sen_devs), use_container_width=True)
+        
+        #st.info('Sum of the commits per chain during a defined time interval from senior developers. As seniors are qualified those who have made their first commit more than 3 years ago.', icon="ℹ️")
+
+    #=======================================
     
     options_commits_all_devs = st.multiselect('Select Blockchains:', all_chains, ['Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
-    
+
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-   
+
         start_date = datetime.today() - timedelta(days=draw_option_timespan(''))
         end_date = datetime.today()      
 
     with col2:
-    
+
         option_agg = st.selectbox(
         'Count of Commits per:',
         ('Week', 'Month'),
@@ -228,33 +283,25 @@ with st.container():
         st.write("")
         
     with col4:
-        st.write("")
-        
+        st.write("")  
 
-st.plotly_chart(multiline_chart_commits_all_devs(option_agg, end_date, start_date, options_commits_all_devs), use_container_width=True)
+    st.plotly_chart(multiline_chart_commits_all_devs(option_agg, end_date, start_date, options_commits_all_devs), use_container_width=True)
 
-st.info('Sum of the commits per chain during a defined time interval', icon="ℹ️")
+    st.info('Sum of the commits per chain during a defined time interval', icon="ℹ️")
 
-#=======================================
+    #=======================================
 
-with st.container():
-
-    options_commits_sen_devs = st.multiselect('Select Blockchains: ', all_chains, ['Bitcoin', 'Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
+    options_new_devs_per_chain = st.multiselect('Select Blockchains:   ', all_chains, ['Bitcoin', 'Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
 
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        
-        start_date = datetime.today() - timedelta(days=draw_option_timespan(' '))
+
+        start_date = datetime.today() - timedelta(days=draw_option_timespan('   '))
         end_date = datetime.today()      
 
     with col2:
-
-        option_agg = st.selectbox(
-        'Count of Commits per: ',
-        ( 'Week', 'Month'),
-        1
-        )
+        st.write("")
         
     with col3:
         st.write("")
@@ -262,63 +309,24 @@ with st.container():
     with col4:
         st.write("")
 
-    st.plotly_chart(multiline_chart_commits_senior_devs(option_agg, end_date, start_date, options_commits_sen_devs), use_container_width=True)
-    
-    st.info('Sum of the commits per chain during a defined time interval from senior developers. As seniors are qualified those who have made their first commit more than 3 years ago.', icon="ℹ️")
+    st.plotly_chart(multiline_chart_new_devs_per_chain(end_date, start_date, options_new_devs_per_chain), use_container_width=True)
 
-#=======================================
+    st.info('Sum of new developers who start to develope on a chain for the first time per month.', icon="ℹ️")
 
-options_new_repositories_per_chain = st.multiselect('Select Blockchains:  ', all_chains, ['Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
-
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    
-    start_date = datetime.today() - timedelta(days=draw_option_timespan('  '))
-    end_date = datetime.today()      
-
-with col2:
     st.write("")
-    
-with col3:
     st.write("")
-    
-with col4:
     st.write("")
 
-st.plotly_chart(multiline_new_repositories_per_chain(end_date, start_date, options_new_repositories_per_chain), use_container_width=True)
+    st.info('More charts coming...', icon="👍")
 
-st.info('Sum of repositories created per chain per month.', icon="ℹ️")
 
-#=======================================
-
-options_new_devs_per_chain = st.multiselect('Select Blockchains:   ', all_chains, ['Bitcoin', 'Ethereum', 'Binance', 'Cardano', 'Solana', 'Polygon'])
-
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-
-    start_date = datetime.today() - timedelta(days=draw_option_timespan('   '))
-    end_date = datetime.today()      
-
-with col2:
-    st.write("")
-    
-with col3:
-    st.write("")
-    
-with col4:
-    st.write("")
-
-st.plotly_chart(multiline_chart_new_devs_per_chain(end_date, start_date, options_new_devs_per_chain), use_container_width=True)
-
-st.info('Sum of new developers who start to develope on a chain for the first time per month.', icon="ℹ️")
+if __name__ == "__main__":
+    main()
 
 
 
-
-        
             
+                
 
             
             
--- pages/3_🗃️_Monitored_Repositories.py
+++ pages/3_🗃️_Monitored_Repositories.py
@@ -5,22 +5,12 @@ import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-from charts import *
-
-
 
  #==========Get Data=============
  
 mongo_client = pymongo.MongoClient("mongodb+srv://admin_nftanalytics:aMMctxcjh580awu8@cluster0.rf3jd.mongodb.net/?retryWrites=true&w=majority")
 
 db = mongo_client["Crypto01"]
-
-df_repositories = pd.DataFrame(db.repositories.find())
-
-df_all_chain = pd.DataFrame(db.chains.find())
-
-all_chains = list(set(df_all_chain['coin']))
-
 
  #==========Create Page=============
  
@@ -30,21 +20,44 @@ st.set_page_config(
     layout="wide",
 )
 
+if 'df_repositories' not in st.session_state:
+    st.session_state.df_repositories = pd.DataFrame(db.repositories.find())
+    
+df_repositories = pd.DataFrame(st.session_state.df_repositories)
+
+def make_clickable(val):
+    # target _blank to open new window
+    return '<a target="_blank" href="{}">{}</a>'.format(val, val)
+
+df_repositories.style.format({'url': make_clickable})
+    
+if 'all_chains' not in st.session_state:
+    df_all_chains = pd.DataFrame(db.chains.find())
+    st.session_state.all_chains = list(set(df_all_chains['coin']))
+    st.session_state.all_chains.append("All")
+    
+all_chains = st.session_state.all_chains
+
+    
+if 'options_list' not in st.session_state:
+    st.session_state.options_list = ["Bitcoin"]
+    
+
 st.title("Monitored GitHub Repositories")
 
-st.header("Total Repositories: " + str(len(df_repositories.index)))
+st.header("Total Repositories: " + str(len(st.session_state.df_repositories.index)))
 
-st.info('Data Mining Methodology: The objective is to provide insights to developer activities on different blockchain protocols. GitHub repositories are herefore screened and assigned to a protocol by a key word algorithm applied to the repository name and description. The key word rules to conclude on what protocol a project is building on, are configured to keep the number of false positive repositories as low as possible.', icon="ℹ️")
+st.info('Data Mining Methodology: The objective is to provide insights to developer activities on different blockchain protocols. GitHub repositories are herefore screened and assigned to a protocol by a key word algorithm applied to the repository name and description.', icon="ℹ️")
 
-all_chains.append("All")
+options = st.multiselect('Select chain', all_chains, ['Ethereum'])
 
-options = st.multiselect('Select chain',all_chains, ["Ethereum"])
+df_repositories_filtered = df_repositories[df_repositories['classification'].isin(options)]
 
-df_repositories = df_repositories[df_repositories['classification'].isin(options)]
-
-fig = go.Figure(data=[go.Table(header=dict(values=['Chain', 'Repository URL']),
-                 cells=dict(values=[df_repositories['classification'], df_repositories['url']]))
+fig = go.Figure(data=[go.Table(columnwidth = [80,400], header=dict(values=['Chain', 'Repository URL']),
+                 cells=dict(values=[df_repositories_filtered['classification'], df_repositories_filtered['url']]))
                      ])
+
 fig.update_layout(width=1000, height=30000)
 
 st.plotly_chart(fig)
+
